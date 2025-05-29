9a2d6588585ff837c0745bfc95059d2141de83f5
Greg Logan <gregorydlogan@gmail.com>
2025-05-29 12:52:16 -0600

    Reworking cleanup script.  We keep the last two builds of the open PRs for opencast/opencast-editor, and cleanup after outselves in terms of removing the downloaded script.  Also amending the gh-pages commit so we don't drag (currently) 3.5G+ of history around for no reason.

--- .github/scripts/cleanup-deployments.sh
+++ .github/scripts/cleanup-deployments.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # That is list the files of type directory whose name does not contain
 # a non-digit. So we only process automated deployments.
 # 
@@ -9,10 +7,14 @@
 # also list files whose name contains sequences of bytes that don't form
 # valid characters in the current locale (like a répertoire encoded in
 # iso8859-1 (mkdir $'r\xe9pertoire') in a locale that uses UTF-8 as charset).
-DEPLOYMENTS=$(LC_ALL=C find . -maxdepth 1 ! -name '*[!0-9]*' -type d -printf '%p\n')
-
+DEPLOYMENTS=$(LC_ALL=C find . -maxdepth 1 ! -name '*[!0-9]*' -type d -printf '%P\n')
 
 for instance in $DEPLOYMENTS
 do
-    find "$instance" -maxdepth 1 -mindepth 1 -type d -printf '%p\n' | sort -g | head -n -3 | xargs rm -rf
+  if [ "OPEN" != "$(gh pr view $instance --repo $1 --json state -q '.state')" ]; then
+    rm -rf $instance
+  else
+    #Keep the last two deploys for each instance that's still open
+    find "$instance" -maxdepth 1 -mindepth 1 -type d -printf '%p\n' | sort -r | tail -n +3 | xargs rm -rf
+  fi
 done
